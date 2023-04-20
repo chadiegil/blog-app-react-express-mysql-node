86c8ce611bc5f0fc9de009cc06dd6f64bf48771c
Chadie Gil <77107717+chadiegil@users.noreply.github.com>
2023-04-20 14:29:05 +0800

    update menu image

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -33,7 +33,8 @@ const Menu = () => {
       <h1>Other posts you may like</h1>
       {posts.map((post) => (
         <div className="post" key={post.id}>
-          <img src={`../upload/${post?.img}`} alt="" />
+          {/* <img src={`../upload/${post?.img}`} alt="" /> */}
+          <img src={`${post?.img}`} alt="" />
           <h2>{post.title}</h2>
           <button>Read More</button>
         </div>
