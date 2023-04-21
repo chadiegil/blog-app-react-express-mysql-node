9f9f87c5479e3c8049fdda8b3f3082300f5a2fa5
Chadie Gil <77107717+chadiegil@users.noreply.github.com>
2023-04-21 11:08:03 +0800

    update api

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,4 +1,6 @@
-const register = (req, res) => {};
+const register = (req, res) => {
+  console.log("login");
+};
 const login = (req, res) => {};
 const logout = (req, res) => {};
 module.exports = { register, login, logout };
